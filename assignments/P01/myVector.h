bdcde84bf1ffaacb6732aa0cb1d2fb7e85059eb5
tarrox1992 <35563248+tarrox1992@users.noreply.github.com>
2018-09-14 01:36:39 -0500

    Update myVector.h

--- assignments/P01/myVector.h
+++ assignments/P01/myVector.h
@@ -1,1 +1,23 @@
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+// class to mimic c++ vector
+
+class myVector {
+private:
+	int *theVector;
+	int *tempV;
+	int *tempP;
+	int oSize;
+	int index;
+	double perFull;
+public:
+	myVector(int size);
+	void pushBack(int item);
+	int popBack();
+	void print();
+	void pushBack(string ohsowrong);
+	int vSize;
+};
